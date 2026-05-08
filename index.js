6cb51b2a345eedf48dbc002bd524d92e7cbd70f2
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-08 08:43:44 +0530

    fetching and displaying
    Displaying colors and their hex value on the screen

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const getClrSchBtn = document.getElementById('get-clr-sch-btn')
+const getClrSchBtn = document.getElementById('get-clr-scheme-btn')
 const seedColor = document.getElementById('seed-color')
 const clrModeSelector = document.getElementById('clr-mode-selector')
+const clrSchemeContainer = document.getElementById('clr-scheme-container')
 
 getClrSchBtn.addEventListener('click', function() {
-    
+    fetch(
+        `https://www.thecolorapi.com/scheme?hex=${(seedColor.value).slice(1)}&mode=${clrModeSelector.value}`
+    ).then(res => res.json())
+    .then(data => {
+       const colorImgs = data.colors.map(color => {
+        return `
+            <div>
+                <img src='${color.image.bare}' alt='' class='clr-img'/>
+                <p class='hex-value'>${color.hex.value}</p>
+            </div>
+        `
+       })
+
+       clrSchemeContainer.innerHTML = colorImgs.join('')
+    })
 })
 
