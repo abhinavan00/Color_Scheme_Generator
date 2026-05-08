3d8eddfdcc8dca2edb8e9f2cf0a287489e08fd78
Abhinav Anand <abhinavanand072@gmail.com>
2026-05-08 08:00:06 +0530

    Color selector design complete

---
+++ index.js
@@ -0,0 +1,8 @@
+const getClrSchBtn = document.getElementById('get-clr-sch-btn')
+const seedColor = document.getElementById('seed-color')
+const clrModeSelector = document.getElementById('clr-mode-selector')
+
+getClrSchBtn.addEventListener('click', function() {
+    
+})
+
